004c6b4a62ea710055e166c46b855dbf1bef8836
Serkan <serkan.ertas.professional@gmail.com>
2025-12-07 15:53:48 +0200

    Create __init__.py

---
+++ models/visual_models/__init__.py
@@ -0,0 +1,5 @@
+# Visual Models Package
+from .visual_model_manager import VisualModelManager
+from .clip_image_embedder import CLIPImageEmbedder
+
+__all__ = ["VisualModelManager", "CLIPImageEmbedder"]
