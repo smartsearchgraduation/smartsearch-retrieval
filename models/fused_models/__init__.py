1dced7971f223e046d00bfb45f1cbd1781defaf3
Serkan <serkan.ertas.professional@gmail.com>
2025-12-07 15:53:53 +0200

    Create __init__.py

---
+++ models/fused_models/__init__.py
@@ -0,0 +1,5 @@
+# Fused Models Package
+from .fused_model_manager import FusedModelManager
+from .clip_fused_embedder import CLIPFusedEmbedder
+
+__all__ = ["FusedModelManager", "CLIPFusedEmbedder"]
